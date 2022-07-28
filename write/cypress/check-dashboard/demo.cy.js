975bfbf0556c4f7840f3c7097795217da3fd6eb0
Frederique Retsema <fretsema@fretsema.nl>
2022-07-28 12:49:34 +0200

    first

--- write/cypress/check-dashboard/demo.cy.js
+++ write/cypress/check-dashboard/demo.cy.js
@@ -1,12 +1,23 @@
-describe('Quick Draft window', () => {
+describe.skip('Quick Draft window', () => {
   beforeEach(() => {
     cy.login()
   })
   
-  it('Test title box', () => {
+  it.skip('Test title box', () => {
     cy.visit('/wp-admin/')
+	cy.get('input#title')
+	  .type('Demo')
+	  .should('have.value', 'Demo')
   })
+  
   it('Test content', () => {
     cy.visit('/wp-admin/')
   })
+
+  it('Click the button', () => {
+    cy.visit('/wp-admin/')
+	
+	cy.contains('Save Draft')
+	  .click()
+  })	
 })
